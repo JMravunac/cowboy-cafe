4dbf8be5c2daed04e35f7188cd505cd090043076
Jack Mravunac <jmravunac@ksu.edu>
2020-02-03 17:00:16 -0600

    WIP Milestone 2

---
+++ Data/Entree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A base class representing a entree
+    /// </summary>
+    public abstract class Entree
+    {
+        /// <summary>
+        /// Gets the price of the side
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// Gets the calories of the entree
+        /// </summary>
+        public abstract uint Calories { get; }
+
+
+        public abstract List<String> SpecialInstructions { get; }
+    }
+}
+
