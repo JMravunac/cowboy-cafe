9a01dc644ffacb9034a80d6a7a8235aceddeaf04
Jack Mravunac <jmravunac@ksu.edu>
2020-03-15 03:31:16 -0500

    POS 3 WIP

--- Data/CornDodgers.cs
+++ Data/CornDodgers.cs
@@ -17,6 +17,62 @@ namespace CowboyCafe.Data
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private Size size = Size.Small;
+
+        /// <summary>
+        /// Size of the bread
+        /// </summary>
+        public override Size Size
+        {
+            get { return this.size; }
+            set
+            {
+                this.size = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Size"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SizeSmall"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SizeMedium"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SizeLarge"));
+            }
+        }
+
+        /// <summary>
+        /// checks if size is small and sets size to small
+        /// </summary>
+        public override bool SizeSmall
+        {
+            get { return Size == Size.Small; }
+            set
+            {
+                Size = Size.Small;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+            }
+        }
+        /// <summary>
+        /// checks if size is medium and sets size to medium
+        /// </summary>
+        public override bool SizeMedium
+        {
+            get { return Size == Size.Medium; }
+            set
+            {
+                Size = Size.Medium;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+            }
+        }
+        /// <summary>
+        /// checks if size is large and sets size to large
+        /// </summary>
+        public override bool SizeLarge
+        {
+            get { return Size == Size.Large; }
+            set
+            {
+                Size = Size.Large;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+            }
+        }
+
         /// <summary>
         /// The calories in the corn dodgers
         /// </summary>
